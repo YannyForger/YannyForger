17df6b719c6fd4c7bf6c321ce865ce3c0e33e7e6
YannyForger <00001100358018sp@al.educacao.sp.gov.br>
2024-08-13 16:53:34 -0300

    adicionando arquivos js e css

---
+++ sktch.js
@@ -0,0 +1,59 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    backgroud(220);
+  }
+  function draw() {
+      background("white");
+      
+      rect(0, 10, 100, 150);
+  }
+  function draw() {
+      background("white");
+  
+      fill("#F34882");
+      rect(0, 10, 100, 150);
+  }
+  function draw() {
+      background("white");
+      
+      stroke("blue") 
+      fill("#F34882") 
+      rect(0, 10, 100, 150)
+  }
+  function draw() {
+    background(220);
+    
+    stroke("blue");
+    fill("#F34882");
+    rect(mouseX, mouseY, 100, 150);
+  }
+  function draw() {
+    background(220);
+    
+    stroke("blue");
+    fill("#F34882");
+    
+    console.log(mouseIsPressed);
+    rect(mouseX, mouseY, 100, 150);
+  }
+  function setup() {
+    // cria uma tela de 400px de largura por 400px de altura
+    createCanvas(400, 400);
+  }
+  function setup() {
+    createCanvas(400, 400);
+    background("white")
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("#F34882");
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
